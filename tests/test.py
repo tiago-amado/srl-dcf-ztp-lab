214f82f0f2bb8bcb1f20e316b92609dd8909b8ab
Tiago Amado <tiago.amado@nokia.com>
2025-10-04 00:19:31 +0100

    Remove clab files

--- tests/test.py
+++
@@ -1,6 +0,0 @@
-from pygnmi.client import gNMIclient
-
-with gNMIclient(target=('clab-dc1-leaf1','57400'), path_cert='./ca.pem', username='admin', password='NokiaSrl1!', debug=True) as gc:
-	result = gc.capabilities()
-	result = gc.get(path=["/system/name/host-name"], encoding="json_ietf")
-#	print(f'[gNMI SERVER] :: {str(result)}')
